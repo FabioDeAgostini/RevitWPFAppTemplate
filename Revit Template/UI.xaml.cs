e56db5d669f061712dbf8f033a7e534ba47fdbb0
FabioDeAgostini <info@studiofada.net>
2020-07-06 08:41:27 +0200

    Initial commit

---
+++ Revit Template/UI.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Windows;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace RevitTemplate
+{
+    /// <summary>
+    /// Interaction logic for UI.xaml
+    /// </summary>
+    public partial class Ui : Window
+    {
+        private readonly Document _doc;
+
+        //private readonly UIApplication _uiApp;
+        //private readonly Autodesk.Revit.ApplicationServices.Application _app;
+        private readonly UIDocument _uiDoc;
+
+        private readonly EventHandlerWithStringArg _mExternalMethodStringArg;
+        private readonly EventHandlerWithWpfArg _mExternalMethodWpfArg;
+
+        public Ui(UIApplication uiApp, EventHandlerWithStringArg evExternalMethodStringArg,
+            EventHandlerWithWpfArg eExternalMethodWpfArg)
+        {
+            _uiDoc = uiApp.ActiveUIDocument;
+            _doc = _uiDoc.Document;
+            //_app = _doc.Application;
+            //_uiApp = _doc.Application;
+            Closed += MainWindow_Closed;
+
+            InitializeComponent();
+            _mExternalMethodStringArg = evExternalMethodStringArg;
+            _mExternalMethodWpfArg = eExternalMethodWpfArg;
+        }
+
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        #region External Project Methods
+
+        private void BExtString_Click(object sender, RoutedEventArgs e)
+        {
+            // Raise external event with a string argument. The string MAY
+            // be pulled from a Revit API context because this is an external event
+            _mExternalMethodStringArg.Raise($"Title: {_doc.Title}");
+        }
+
+        private void BExternalMethod1_Click(object sender, RoutedEventArgs e)
+        {
+            // Raise external event with this UI instance (WPF) as an argument
+            _mExternalMethodWpfArg.Raise(this);
+        }
+
+        #endregion
+
+        #region Non-External Project Methods
+
+        private void UserAlert()
+        {
+            //TaskDialog.Show("Non-External Method", "Non-External Method Executed Successfully");
+            MessageBox.Show("Non-External Method Executed Successfully", "Non-External Method");
+
+            //Dispatcher.Invoke(() =>
+            //{
+            //    TaskDialog mainDialog = new TaskDialog("Non-External Method")
+            //    {
+            //        MainInstruction = "Hello, Revit!",
+            //        MainContent = "Non-External Method Executed Successfully",
+            //        CommonButtons = TaskDialogCommonButtons.Ok,
+            //        FooterText = "<a href=\"http://usa.autodesk.com/adsk/servlet/index?siteID=123112&id=2484975 \">"
+            //                     + "Click here for the Revit API Developer Center</a>"
+            //    };
+
+
+            //    TaskDialogResult tResult = mainDialog.Show();
+            //    Debug.WriteLine(tResult.ToString());
+            //});
+        }
+
+        private void BNonExternal3_Click(object sender, RoutedEventArgs e)
+        {
+            // the sheet takeoff + delete method won't work here because it's not in a valid Revit api context
+            // and we need to do a transaction
+            // Methods.SheetRename(this, _doc); <- WON'T WORK HERE
+            UserAlert();
+        }
+
+        private void BNonExternal1_Click(object sender, RoutedEventArgs e)
+        {
+            Methods.DocumentInfo(this, _doc);
+            UserAlert();
+        }
+
+        private void BNonExternal2_Click(object sender, RoutedEventArgs e)
+        {
+            Methods.WallInfo(this, _doc);
+            UserAlert();
+        }
+
+        #endregion
+    }
+}
